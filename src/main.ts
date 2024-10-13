9ec152d45aeff5e08f64e48d81f1957e545c09a4
anmolkalsi1 <w08444988@myscc.ca>
2024-10-12 23:40:14 -0400

    defined a list of routes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { Routes } from '@angular/router';
+import { ContentListComponent} from "./app/content-list/content-list.component";
+import { ContentListItemComponent} from "./app/content-list-item/content-list-item.component";
+import { ModifyRestaurantComponent} from "./app/modify-restaurant/modify-restaurant.component";
+import { PageNotFoundComponent} from "./app/page-not-found/page-not-found.component";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+export const routes: Routes = [
+  { path: '', redirectTo: '/restaurants', pathMatch: 'full' },
+  { path: 'restaurant', component: ContentListItemComponent },
+  { path: 'restaurant/:id', component:  ContentListComponent},
+  { path: 'modify-restaurant', component: ModifyRestaurantComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
