7dc0590d61a7ee7911f0ee2a698ec90ca5d6b76e
anmolkalsi1 <w08444988@myscc.ca>
2024-11-02 21:15:10 -0400

    added buttons for editing and deleteing items

--- src/app/content-list/content-list.component.ts
+++ src/app/content-list/content-list.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';  // Import OnInit
-import { ContentListItemComponent } from '../content-list-item/content-list-item.component';
-import { NgClass, NgForOf, NgStyle } from "@angular/common";
-import { Restaurants } from "../restaurants";
-import { RestaurantService } from "../services/restaurant.service";  // Import the service
+import { Router } from '@angular/router';
+import {Component, OnInit} from "@angular/core";
+import {ContentListItemComponent} from "../content-list-item/content-list-item.component";
+import {NgClass, NgForOf, NgStyle} from "@angular/common";
+import {Restaurants} from "../restaurants";
+import {RestaurantService} from "../services/restaurant.service";  // Import Router
 
 @Component({
   selector: 'app-content-list',
@@ -14,14 +15,27 @@ import { RestaurantService } from "../services/restaurant.service";  // Import t
 export class ContentListComponent implements OnInit {
   contentItems: Restaurants[] = [];
 
-  constructor(private restaurantService: RestaurantService) {}
-
+  constructor(private restaurantService: RestaurantService, private router: Router) {}
 
   ngOnInit(): void {
     this.restaurantService.getRestaurants().subscribe({
-      next: (data: Restaurants[]) =>this.contentItems = data,
-      error:err => console.error("Error fetching restaurant", err),
-      complete:() => console.log("restaurant data fetch complete!")
+      next: (data: Restaurants[]) => this.contentItems = data,
+      error: err => console.error("Error fetching restaurant", err),
+      complete: () => console.log("Restaurant data fetch complete!")
+    });
+  }
+
+  editRestaurant(item: number) {
+    this.router.navigate(['/modify'], { state: { restaurant: item } });
+  }
+
+  deleteRestaurant(id: number) {
+    this.restaurantService.removeRestaurant(id).subscribe({
+      next: () => {
+        this.contentItems = this.contentItems.filter(restaurant => restaurant.id !== id);
+        console.log("Restaurant deleted successfully");
+      },
+      error: err => console.error("Error deleting restaurant", err)
     });
   }
 }
